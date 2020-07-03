1f81609c36bd68bd4cfdca16c9fbb701913573ba
junyeopN <junyeop0902@snu.ac.kr>
2020-07-03 13:14:19 +0900

    Made Base Index.html

--- rectangleanimation.js
+++ rectangleanimation.js
@@ -1,10 +1,11 @@
 var canvas = document.getElementById("ballanimation");
 var ctx = canvas.getContext("2d");
 
-
+var WIDTH = 800;
+var HEIGHT = 100;
 
 var x1 = 10;
-var y1 = 10;
+var y1 = 30;
 var x2 = 80;
 var y2 = 20;
 
@@ -15,7 +16,11 @@ var dx = 1;
 
 var animationIntervalId = -1;
 
+ctx.canvas.width = WIDTH;
+ctx.canvas.height = HEIGHT;
+
 function draw() {
+    
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
     ctx.rect(x1, y1, x2, y2);
